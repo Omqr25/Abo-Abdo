002c33a2f8539fe5134e6570f8d4356b8687973b
OmarMidanii <omarmidani90@gmail.com>
2024-11-23 12:34:47 +0200

    minor fix

--- Magic of East/routes/api.php
+++ Magic of East/routes/api.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
     Route::post('register', 'register');
-    Route::post('logout', 'logout');
+    Route::post('logout', 'logout')->middleware('auth:sanctum');
 });
 
 Route::get('/', function () {
