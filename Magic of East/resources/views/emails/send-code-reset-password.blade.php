af6c7d48ce6ee97e2c47ffc60eb0ed1e44c479b6
GhinaAlHarash <dramaqueen3111@gmail.com>
2025-05-14 17:40:51 +0300

    edit

--- Magic of East/resources/views/emails/send-code-reset-password.blade.php
+++ Magic of East/resources/views/emails/send-code-reset-password.blade.php
@@ -8,7 +8,7 @@
 <body>
     <p>Hello,</p>
     <p>You recently requested to reset your password. Use the code below to complete the process:</p>
-    <div>{{code}}</div>
+    <div>{{$code}}</div>
     <p>If you did not request this, please ignore this email or contact support if you have concerns.</p>
     <p>Thank you</p>
 </body>
